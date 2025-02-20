1022897950a48fd78c294d5023ee0a4fe11f82d4
alexsa <lexafitriayu@gmail.com>
2025-02-20 10:14:39 +0700

    Add files via upload

---
+++ Mahasiswa02.java
@@ -0,0 +1,54 @@
+public class Mahasiswa02 {
+    String nama;
+    String nim;
+    String kelas;
+    Double ipk;
+
+    void tampilkanInformasi () {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas (String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk (Double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+        ipk = ipkBaru;
+        } else{
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
+    }
+
+    String ninaiKinerja () {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return"Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return"Kinerja cukup";
+        } else {
+            return"kinerja kurang";
+        }
+    }
+
+    public Mahasiswa02 () {
+
+    }
+
+    public Mahasiswa02 (String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
+    public static void main(String[] args) {
+        
+    Mahasiswa02 mhs = new Mahasiswa02 ("Alexsa Fitria Ayu", "244107020020", 3.75, "TI 1E");
+    mhs.tampilkanInformasi();
+    }
+}
